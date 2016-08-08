094cc34c0000f43991fe49df3213afae1f77e6ff
Jan Haering <hhj@centrum.cz>
2016-08-08 16:06:02 +0200

    feat: material icon tests

--- src/native/shoplist/ShoplistPage.react.js
+++ src/native/shoplist/ShoplistPage.react.js
@@ -1,6 +1,22 @@
 /** Created by hhj on 8/8/16. */
 import React, { PropTypes } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { IconToggle, Icon }  from 'react-native-material-design'
 import { CenteredContainer, Text } from '../app/components'
+
+const style = StyleSheet.create({
+  column: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+});
 
 export default class ShoplistPage extends React.Component {
   static propTypes = {};
@@ -9,9 +25,21 @@ export default class ShoplistPage extends React.Component {
 
   render() {
     return (
-      <CenteredContainer>
-        <Text>Jo!</Text>
-      </CenteredContainer>
+      <View style={style.column}>
+        <View style={[style.row, { flex: 2 }]}>
+          <IconToggle color="red300">
+            <Icon name="golf-course" />
+          </IconToggle>
+        </View>
+        <View style={style.row}>
+          <IconToggle color="red300">
+            <Icon name="call" />
+          </IconToggle>
+          <IconToggle color="red300">
+            <Icon name="pause" />
+          </IconToggle>
+        </View>
+      </View>
     )
   }
 }
